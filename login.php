b21988b242db6bdb84d12f88dc5e97e6cb25d7e9
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-07-13 11:19:18 +0900

    ajax部分を除いたログイン機能　push

--- login.php
+++ login.php
@@ -1,27 +1,8 @@
-<?php
-  $login_id = $_POST['login_id'];
-  $login_password = $_POST['login_password'];
-  // DB接続ファイルを読み込み
-  require_once 'db_connect.php';
-  $stmt = $db -> query("SELECT * FROM user_info");
-  // DBから取得したデータをループ処理で表示
-  while($line = $stmt -> fetch(PDO::FETCH_ASSOC)) {
-    // try {
-      if ($login_id == $line['login_id'] AND $login_password == $line['login_password']) {
-        setcookie("login_State", $line['user_name'], time()+(3 * 24 * 60 * 60));
-        echo "ログイン成功しました。<br>";
-        echo $line['user_id'] . "様<br>";
-        break;
-      } 
-
-    //   else {
-    //     throw new Exception("ログイン失敗しました。");
-    //   }
-    // } catch(Exception $e) {
-    //   die($e -> getMessage());
-    // }
-}
-?>
+<?php if (isset($_COOKIE['login_State'])) : ?>
+  <p>ログイン成功しました。</p>
+  <p><?php echo $_COOKIE['login_State']; ?>様</p>
+  <?php exit; ?>
+<?php endif; ?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -31,13 +12,29 @@
   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<form action="login.php" method="POST">
+<form action="login_check.php" method="POST">
   ID：<input type="text" name="login_id">
   パスワード：<input type="text" name="login_password">
   <input type="submit" value="ログイン">
+  <div id="result"></div>
 </form>
 </body>
 </html>
-<?php
-  echo $_COOKIE['user_id'];
-?>
+<!-- <script type="text/javascript">
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    var result = document.getElementById('result');
+    if (req.readyState == 4) { // 通信の完了時
+      if (req.status == 200) { // 通信の成功時
+        result.innerHTML = req.responseText;
+      }
+    }else{
+      result.innerHTML = "通信中...";
+    }
+  }
+
+  request.open('POST', 'helloAjax.php', true);
+  request.setRequestHeader('content-type',
+  'application/x-www-form-urlencoded;charset=UTF-8');
+  request.send('name=' + encodeURIComponent(document.fm.name.value));
+</script> -->
---
+++ login_check.php
@@ -0,0 +1,37 @@
+<?php
+  // ログインID
+  $login_id = $_POST['login_id'];
+  // ログインパスワード
+  $login_password = $_POST['login_password'];
+  // DB接続ファイルを読み込み
+  require_once 'db_connect.php';
+  // ログイン情報テーブルから全てのデータを取得
+  $stmt = $db -> query("SELECT * FROM user_info");
+  // DBから取得したデータをループ処理で表示
+  try {
+    while($line = $stmt -> fetch(PDO::FETCH_ASSOC)) {
+      // ログインID、ログインパスワードがDBに登録されているログイン情報と一致しているか判断
+      // 一致している場合
+      if ($login_id == $line['login_id'] AND $login_password == $line['login_password']) {
+        // ログイン成功情報をcookieに送信
+        setcookie("login_State", $line['user_name'], time()+(3 * 24 * 60 * 60));
+        // ログイン情報を入れる
+        $check_var = $line['user_name'];
+        // ログイン画面を表示
+        require_once 'login.html';
+        echo "ログイン成功しました。<br>";
+        echo $line['user_name'] . "様<br>";
+      }
+    }
+    // $check_varにログイン情報がセットされていない場合
+    if (!isset($check_var)) {
+      // エラー処理を投げる
+      throw new Exception("ログイン失敗しました。");
+    }
+    // catch関数で発生した例外を処理
+  } catch(Exception $e) {
+    // エラーメッセージを表示し、プログラムを強制終了
+    die($e -> getMessage());
+  }
+?>
+
