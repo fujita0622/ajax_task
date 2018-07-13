1438c3017fa6615157f26d18eedfa2a186b035b6
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-07-13 13:50:13 +0900

    login.php commit

--- login.php
+++ login.php
@@ -10,31 +10,41 @@
   <title>ログイン画面</title>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="css/style.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 <body>
-<form action="login_check.php" method="POST">
-  ID：<input type="text" name="login_id">
-  パスワード：<input type="text" name="login_password">
-  <input type="submit" value="ログイン">
-  <div id="result"></div>
+<form method="post" accept-charset="utf-8">
+  ID：<input type="text" name="login_id" id="login_id">
+  パスワード：<input type="text" name="login_password" id="login_password">
 </form>
+  <button id="ajax">ログイン</button>
+  <div class="result"></div>
 </body>
 </html>
-<!-- <script type="text/javascript">
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    var result = document.getElementById('result');
-    if (req.readyState == 4) { // 通信の完了時
-      if (req.status == 200) { // 通信の成功時
-        result.innerHTML = req.responseText;
+<script type="text/javascript">
+  $(function(){
+    // ログインボタン(#ajax)を押した時に実行する関数を定義
+    $('#ajax').on('click',function(){
+      // Ajax通信を$.ajaxメソッド内に定義
+      $.ajax({
+        // アクセス先のパス：login_check.php
+        url:'./login_check.php',
+        // 通信に利用するHTTPメソッド：POST
+        type:'POST',
+        // 送信するデータ
+        data:{
+          'login_id':$('#login_id').val(),
+          'login_password':$('#login_password').val()
       }
-    }else{
-      result.innerHTML = "通信中...";
     }
-  }
-
-  request.open('POST', 'helloAjax.php', true);
-  request.setRequestHeader('content-type',
-  'application/x-www-form-urlencoded;charset=UTF-8');
-  request.send('name=' + encodeURIComponent(document.fm.name.value));
-</script> -->
+    // Ajaxリクエストが成功した時発動
+    .done((data) {
+      $('.result').html('OK');
+    })
+    // Ajaxリクエストが失敗した時発動
+    .fail((data) {
+      $('.result').html('NO');
+    })
+    });
+  });
+ </script>
